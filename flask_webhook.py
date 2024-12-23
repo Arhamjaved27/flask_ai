b4f2e3cb7a574e0afd72b77b8cf91db13d3a6066
Arham Javed <126825310+Arhamjaved27@users.noreply.github.com>
2024-12-24 02:38:21 +0500

    Add files via upload

---
+++ flask_webhook.py
@@ -0,0 +1,16 @@
+from flask import Flask, request
+from apps import process_chatbot_message  # Import function from main.py
+
+app = Flask(__name__)
+
+@app.route('/whatsapp', methods=['POST'])
+def whatsapp_webhook():
+    incoming_message = request.form.get('Body')
+    sender = request.form.get('From')
+
+    # response = process_chatbot_message(incoming_message)  # Call your chatbot logic
+    # return f"<Response><Message>{response}</Message></Response>"
+    return f"<Response><Message>{incoming_message}</Message></Response>"
+
+if __name__ == '__main__':
+    app.run(port=5000)
