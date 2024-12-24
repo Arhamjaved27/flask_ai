5afec365380b42a0a70db7312fbb4514dea36c9c
Arham Javed <126825310+Arhamjaved27@users.noreply.github.com>
2024-12-24 14:48:08 +0000

    update flask

--- flask_webhook.py
+++ flask_webhook.py
@@ -15,7 +15,7 @@ def whatsapp_webhook():
 
     # response = process_chatbot_message(incoming_message)  # Call your chatbot logic
     # return f"<Response><Message>{response}</Message></Response>"
-    return f"<Response><Message>{incoming_message}</Message></Response>"
+    return f"<Response><Message>{incoming_message +"hello"}</Message></Response>"
 
 if __name__ == '__main__':
     app.run(port=5000)
