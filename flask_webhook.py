87b3713efa6fb39db3c626d852e566d9d9b3e72a
Arham Javed <126825310+Arhamjaved27@users.noreply.github.com>
2024-12-24 15:56:54 +0000

    update

--- flask_webhook.py
+++ flask_webhook.py
@@ -14,7 +14,6 @@ def whatsapp_webhook():
     sender = request.form.get('From')
 
     Message = "This is "
-    # Message = "This is "
     process_chatbot_message(Message)
     # response = process_chatbot_message(incoming_message)  # Call your chatbot logic
     # return f"<Response><Message>{response}</Message></Response>"
