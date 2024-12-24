514f5dc38977511d170685f3674da5435e84062b
Arham Javed <126825310+Arhamjaved27@users.noreply.github.com>
2024-12-24 14:29:40 +0000

    Flask_webhook update

--- flask_webhook.py
+++ flask_webhook.py
@@ -3,19 +3,19 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def whatsapp_webhook():
-    return 'Hello World'
-
-
-# @app.route('/whatsapp', methods=['POST'])
+# @app.route('/')
 # def whatsapp_webhook():
-#     incoming_message = request.form.get('Body')
-#     sender = request.form.get('From')
+#     return 'Hello World'
 
-#     # response = process_chatbot_message(incoming_message)  # Call your chatbot logic
-#     # return f"<Response><Message>{response}</Message></Response>"
-#     return f"<Response><Message>{incoming_message}</Message></Response>"
+
+@app.route('/whatsapp', methods=['POST'])
+def whatsapp_webhook():
+    incoming_message = request.form.get('Body')
+    sender = request.form.get('From')
+
+    # response = process_chatbot_message(incoming_message)  # Call your chatbot logic
+    # return f"<Response><Message>{response}</Message></Response>"
+    return f"<Response><Message>{incoming_message}</Message></Response>"
 
 if __name__ == '__main__':
     app.run(port=5000)
